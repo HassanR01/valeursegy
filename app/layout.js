8ec92faa1ff662281f89301a497d74b5640aba5f
hassanr01 <hassanrageh.236@gmail.com>
2024-07-14 12:13:19 +0300

    landing

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-mainColor">
+      <body className="bg-mainColor h-screen">
         {children}
       </body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    
+      <div className="image w-full h-full flex flex-col items-center justify-center">
+        <Image src={'/underctu.png'} width={300} height={300} alt="Under Construction" />
+        <h2 className="text-2xl font-bold text-white mt-5 mb-2">Team is already working to take your idea from the vision to the reality</h2>
+        <p className="text-2xl font-bold text-white"></p>
+    </div>
     </>
   );
 }
