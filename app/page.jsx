1dada57c304ac48a0a7e188270bcbf52d30f71a8
hassanr01 <hassanrageh.236@gmail.com>
2024-07-19 23:19:14 +0300

    edits

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,24 @@
 'use client'
-import { Environment, OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Model } from "./components/models/Env";
+import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Env } from "./components/models/Env";
+import { Suspense, useRef } from "react";
+import * as THREE from "three"
 
 export default function Home() {
+
   return (
     <>
       <div className="image w-full h-[100vh] flex flex-col items-center justify-center">
-        <Canvas>
-          <Environment preset="city" />
-          <OrbitControls />
-          <Model />
+        <Canvas shadows>
+          <PerspectiveCamera makeDefault position={[0, 2.5, 2.5]} />
+          <OrbitControls maxDistance={3.5} minDistance={0.8} maxPolarAngle={0.8} />
+
+          <directionalLight args={["#ffffff", 2]} position={[0, 2.5,2.5]} shadow={1} />
+          
+          <Environment preset="apartment" />
+          <Env />
+
         </Canvas>
       </div>
     </>
