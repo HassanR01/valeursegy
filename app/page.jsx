f9e383e28b777ae63e0a20336b25a7f161276030
hassanr01 <hassanrageh.236@gmail.com>
2024-07-20 01:10:45 +0300

    try

--- app/page.jsx
+++ app/page.jsx
@@ -12,12 +12,19 @@ export default function Home() {
       <div className="image w-full h-[100vh] flex flex-col items-center justify-center">
         <Canvas shadows>
           <PerspectiveCamera makeDefault position={[0, 2.5, 2.5]} />
-          <OrbitControls maxDistance={3.5} minDistance={0.8} maxPolarAngle={0.8} />
+          <OrbitControls maxDistance={3.5} minDistance={0.8} maxPolarAngle={1.2} />
 
-          <directionalLight args={["#ffffff", 2]} position={[0, 2.5,2.5]} shadow={1} />
-          
-          <Environment preset="apartment" />
+          <directionalLight args={["#ffffff", 2]} position={[0, 5, 0]} shadow={5} />
+          <ambientLight args={["#ffffff", 3]} />
+
           <Env />
+
+          <Environment background>
+            <mesh scale={100}>
+              <sphereGeometry args={[50, 100, 100]} />
+              <meshBasicMaterial side={THREE.BackSide} color="#0d1067" />
+            </mesh>
+          </Environment>
 
         </Canvas>
       </div>
