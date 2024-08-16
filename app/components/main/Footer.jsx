6e70dd0c01d1fa31303507679d9e825f33ae4e63
hassanr01 <hassanrageh.236@gmail.com>
2024-08-16 23:14:59 +0300

    edit Footer

--- app/components/main/Footer.jsx
+++ app/components/main/Footer.jsx
@@ -37,7 +37,7 @@ export default function Footer() {
                 </ul>
             </div>
             <div className="links flex my-8 flex-col items-center text-center lg:text-start lg:items-start justify-center z-10">
-                <h2 className='text-2xl font-bold text-mainColor mb-4'>Be a one of the family to be updated</h2>
+                <h2 className='text-2xl font-bold text-mainColor mb-4'>Be one of the family to keep you updated</h2>
                 <div className="input lg:min-w-[450px]">
                     <input type="email" name="email" id="email" placeholder='Enter Your Email' />
                 </div>
