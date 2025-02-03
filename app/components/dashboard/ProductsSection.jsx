bfbff930386b7cfa099144caf339ed56b5660fe5
hassanr01 <hassanrageh.236@gmail.com>
2025-02-03 16:26:44 +0200

    edit in dashboard

--- app/components/dashboard/ProductsSection.jsx
+++ app/components/dashboard/ProductsSection.jsx
@@ -2,6 +2,7 @@
 import { motion } from 'framer-motion'
 import HTMLReactParser from 'html-react-parser';
 import dynamic from 'next/dynamic'
+import Link from 'next/link';
 const JoditEditor = dynamic(() => import('jodit-react'), { ssr: false });
 import React, { useRef, useState } from 'react'
 
@@ -242,18 +243,18 @@ export default function ProductsSection({ products }) {
                   <div className='absolute top-0 right-0 flex flex-col items-center justify-center'>
 
                     <div onClick={() => deleteSection(ind)} className="delete m-1 p-3 cursor-pointer bg-whiteColor rounded-xl text-red-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg></div>
-                    <div onClick={() => {
+                    <Link href={"#editEn"} onClick={() => {
                       setEditSection(true)
                       setSubTitle(section.subTitle)
                       setText(section.text)
                       setSectionIndex(ind)
-                    }} className="edit m-1 p-3 cursor-pointer bg-whiteColor rounded-xl text-green-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /> </svg></div>
+                    }} className="edit m-1 p-3 cursor-pointer bg-whiteColor rounded-xl text-green-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /> </svg></Link>
                   </div>
                 </div>
               ))}
             </div>
 
-            <div className="sectionAdd my-4 w-full px-2">
+            <div id='editEn' className="sectionAdd my-4 w-full px-2">
               <div className="subtitle my-4">
                 <label htmlFor="subTitle">Sub Title for Section: </label>
                 <input type="text" name="subtitle" id="subTitle" placeholder='SubTitle for Section in Product' value={subTitle} onChange={(e) => setSubTitle(e.target.value)} />
@@ -285,19 +286,19 @@ export default function ProductsSection({ products }) {
                   <p className='w-[90%]'>{HTMLReactParser(section.textAr)}</p>
                   <div className='absolute top-0 left-0 flex flex-col items-center justify-center'>
                     <div onClick={() => deleteSectionAr(ind)} className="delete p-3 m-1 cursor-pointer bg-whiteColor rounded-xl text-red-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg></div>
-                    <div onClick={() => {
+                    <Link href={'#editAr'} onClick={() => {
                       setEditSectionAr(true)
                       setSubTitleAr(section.subTitleAr)
                       setTextAr(section.textAr)
                       setSectionIndexAr(ind)
                     }}
-                      className="edit p-3 m-1 cursor-pointer bg-whiteColor rounded-xl text-green-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /> </svg></div>
+                      className="edit p-3 m-1 cursor-pointer bg-whiteColor rounded-xl text-green-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /> </svg></Link>
                   </div>
                 </div>
               ))}
             </div>
 
-            <div className="sectionAddAr my-4 w-full px-2">
+            <div id='editAr' className="sectionAddAr my-4 w-full px-2">
               <div className="subtitle my-4">
                 <label htmlFor="subTitle">Sub Arabic Title for Section: </label>
                 <input type="text" name="subtitle" id="subTitle" placeholder='Arabic subTitle for Section in Product' value={subTitleAr} onChange={(e) => setSubTitleAr(e.target.value)} />
